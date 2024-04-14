6cc987773f5e1f7299eaa3d2568911bf8078cb01
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-04-15 01:05:07 +0530

    added div with image and paragraph to App

--- src/App.py
+++ src/App.py
@@ -3,7 +3,12 @@ from reactpy import component, html, run
 
 @component
 def App():
-    return html.h1("Hello from ReactPy!")
+    return html.div(
+        html.img({
+            "src": "https://raw.githubusercontent.com/reactive-python/reactpy/main/branding/svg/reactpy-logo-square.svg"
+        }),
+        html.p('Edit src/App.py and save to reload.')
+    )
 
 
 run(App)
