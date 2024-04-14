e85c1e083f785879720b401c1a77de59f14a6750
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-04-15 00:36:06 +0530

    fixed main script

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+from src import App
+
+from flask import Flask
+
 from reactpy import component, html
 from reactpy.backend.flask import configure
-
-
-@component
-def App():
-    return html.h1("Hello, world!")
 
 
 app = Flask(__name__)
