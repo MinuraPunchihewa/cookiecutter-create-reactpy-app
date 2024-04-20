9c4af2bbc3f3ea8819bdd6e88f2f7aebff47ceaa
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-04-21 01:00:25 +0530

    updated the script to not install deps

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-poetry init --name "{{ cookiecutter.app_name }}"
+poetry init --name "{{ cookiecutter.app_name }}" --dependency "no" --dev-dependency "no"
 poetry add reactpy[flask] gunicorn
