1f7e691cda8d038d25818d147744a30b09d8492d
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-04-21 16:58:23 +0530

    added support for fastapi and starlette backends: script

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+backend="{{ cookiecutter.backend }}"
+server=""
+
+if [ "$backend" == "flask" ]; then
+    server="gunicorn"
+elif [ "$backend" == "fastapi" ] || [ "$backend" == "starlette" ]; then
+    server="uvicorn"
+else 
+    echo "Backend not supported!"
+    exit 1
+fi
+
 # TODO: Add validation for the and Python version
 # TODO: Add authors and licence
-# TODO: Add support for other backends
+# TODO: Add support for other backends: Sanic and Tornado
 poetry init \
     --name "{{ cookiecutter.app_name }}" \
     --description "{{ cookiecutter.description }}" \
     --python "{{ cookiecutter.python_version }}" \
-    --dependency "reactpy[flask]" \
-    --dependency "gunicorn" \
+    --dependency "reactpy[${backend}]" \
+    --dependency "${server}" \
     --no-interaction
