b16a1e51206c0fefc7963a8eb64ac536dcbf93cc
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-04-23 02:00:59 +0530

    removed use of poetry from post gen script and added requirements.txt file

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# TODO: Add support for other backends: Sanic and Tornado
 backend="{{ cookiecutter.backend }}"
 server=""
 
@@ -12,13 +13,5 @@ else
     exit 1
 fi
 
-# TODO: Add validation for the and Python version
-# TODO: Add authors and licence
-# TODO: Add support for other backends: Sanic and Tornado
-poetry init \
-    --name "{{ cookiecutter.app_name }}" \
-    --description "{{ cookiecutter.description }}" \
-    --python "{{ cookiecutter.python_version }}" \
-    --dependency "reactpy[${backend}]" \
-    --dependency "${server}" \
-    --no-interaction
+echo "reactpy[$backend]" >> requirements.txt
+echo "$server" >> requirements.txt
