0b1e27188a8c4acd4d9d7bf8032460b44cbb0d2a
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-04-29 01:32:01 +0530

    updated App with the correct structure and styling

--- {{cookiecutter.project_directory}}/src/App.py
+++ {{cookiecutter.project_directory}}/src/App.py
@@ -4,8 +4,47 @@ from reactpy import component, html
 @component
 def App():
     return html.div(
-        html.img({
-            "src": "/public/logo.svg"
-        }),
-        html.p('Edit src/App.py and save to reload.')
+        {
+            "style": {
+                "textAlign": "center",
+            }
+        },
+        html.header(
+            {
+                "style": {
+                    "backgroundColor": "#282c34",
+                    "minHeight": "100vh",
+                    "display": "flex",
+                    "flexDirection": "column",
+                    "alignItems": "center",
+                    "justifyContent": "center",
+                    "fontSize": "calc(10px + 2vmin)",
+                    "color": "white",
+                }
+            },
+            html.img({
+                "src": "/public/logo.svg",
+                "style": {
+                    "height": "40vmin",
+                    "pointerEvents": "none",
+                }
+            }),
+            html.p(
+                'Edit ',
+                html.code('src/App.js'),
+                ' and save to reload.'
+            ),
+            html.a(
+                {
+                    "className": "App-link",
+                    "href": "https://reactpy.dev/docs/index.html",
+                    "target": "_blank",
+                    "rel": "noopener noreferrer",
+                    "style": {
+                        "color": "#61dafb"
+                    }
+                },
+                'Learn ReactPy'
+            )
+        )
     )
