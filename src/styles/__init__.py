03fe6a9402dbf21e6340d80ab19ae42fdb1e6552
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-04-29 23:04:54 +0530

    moved styles to separate files in styles dir

---
+++ src/styles/__init__.py
@@ -0,0 +1,2 @@
+from .App import App, App_header, App_link, App_logo
+from .index import body, code
---
+++ src/styles/index.py
@@ -0,0 +1,8 @@
+body = {
+    "margin": "-8px",
+    "fontFamily": "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue'"
+}
+
+code = {
+    "fontFamily": "source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace"
+}
